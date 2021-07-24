89fa6458413523068f87ebc9d76f167057073561
Moshe-1 <lesejanemoshe11@gmail.com>
2021-07-24 14:16:48 +0200

    Included bootstrap Listing F1 world champions

--- mobiquity-f1-championship-app/src/app/app.component.ts
+++ mobiquity-f1-championship-app/src/app/app.component.ts
@@ -1,21 +1,29 @@
 import { Component } from '@angular/core';
 import {MobiquityF1ChampionshipServicesService} from "./mobiquity-f1-championship-services.service";
 import {WorldChampions} from "./models/worldChampions";
+import {WinnersOfTheYear} from "./models/winnersOfTheYear";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'mobiquity-f1-championship-app';
   worldChampions: WorldChampions;
+  winnersOfTheYear: WinnersOfTheYear;
   constructor(private championshipServicesService: MobiquityF1ChampionshipServicesService) {
 
   }
   getWorldChampions(): void {
     this.championshipServicesService.getWorldChampions().subscribe((response: WorldChampions) => {
       this.worldChampions = response;
+    })
+  }
+
+  getWinnersOfSelectedYear(year): void {
+    this.championshipServicesService.getWinnersOfSelectedYear(year).subscribe((response: WinnersOfTheYear) => {
+      this.winnersOfTheYear = response;
     })
   }
 
--- mobiquity-f1-championship-app/src/app/mobiquity-f1-championship-services.service.ts
+++ mobiquity-f1-championship-app/src/app/mobiquity-f1-championship-services.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {WorldChampions} from "./models/worldChampions";
+import {WinnersOfTheYear} from "./models/winnersOfTheYear";
 @Injectable({
   providedIn: 'root'
 })
@@ -10,11 +11,14 @@ export class MobiquityF1ChampionshipServicesService {
   }
 
   getWorldChampions() {
-
     //Defining a param that will be passed to the API during the get() call
     //this is to filter the response for driver standings from 2005 until present
     const params = new HttpParams().append('offset', '55');
-    //Using HttpClient get() method to make an API call of F1 world champions.
+    //Using HttpClient get() method to make an API call/request of F1 world champions.
     return this.http.get<WorldChampions>('https://ergast.com/api/f1/driverStandings/1.json', {params});
+  }
+
+  getWinnersOfSelectedYear(year){
+    return this.http.get<WinnersOfTheYear>(' https://ergast.com/api/f1/' + year + '/results/1.json');
   }
 }
