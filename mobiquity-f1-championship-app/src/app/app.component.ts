62b5358445bf0d984b29805d8f0a003590e05db6
Moshe-1 <lesejanemoshe11@gmail.com>
2021-07-26 09:08:41 +0200

    moved services to the service directory

--- mobiquity-f1-championship-app/src/app/app.component.ts
+++ mobiquity-f1-championship-app/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {MobiquityF1ChampionshipServicesService} from "./mobiquity-f1-championship-services.service";
+import {MobiquityF1ChampionshipServicesService} from "./services/mobiquity-f1-championship-services.service";
 import {WorldChampions} from "./models/worldChampions";
 import {WinnersOfTheYear} from "./models/winnersOfTheYear";
 import {trigger,state,style,animate,transition} from '@angular/animations';
--- mobiquity-f1-championship-app/src/app/services/mobiquity-f1-championship-services.service.ts
+++ mobiquity-f1-championship-app/src/app/services/mobiquity-f1-championship-services.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
-import {WorldChampions} from "./models/worldChampions";
-import {WinnersOfTheYear} from "./models/winnersOfTheYear";
+import {WorldChampions} from "../models/worldChampions";
+import {WinnersOfTheYear} from "../models/winnersOfTheYear";
 @Injectable({
   providedIn: 'root'
 })
